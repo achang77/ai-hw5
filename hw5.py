dfd3c8a164bc0a6e9bd610e23cc488c79148e593
romanZwang <rwang2006@yahoo.com>
2018-05-06 19:21:41 -0400

    Replaced tabs with 4 spaces for usage in Pycharm Added dummy return statements and fixed indenting so the code is compile-able

--- hw5.py
+++ hw5.py
@@ -1,15 +1,11 @@
 from scipy.stats import beta
-data= 
-{
+data= {
 "slots": [ [ [], [], [] ] for x in range(100) ],
 "last-slot":None,
 "ct":0,
 "our_utility":1000000,
 "end-explore":0
 }
-
-
-
 
 """
 Go through first 20 slot machines 30 pulls each to generate data 
@@ -22,8 +18,7 @@ data["slots"][11][2][0] = payoff
 
 
 
-state = 
-{"team-code": "eef8976e",
+state = {"team-code": "eef8976e",
 "game": "phase_1",
 "pulls-left": 99999,
 "last-cost": 0.75,
@@ -32,75 +27,74 @@ state =
 
 
 def phase1(state):
+    last_slot = data["last-slot"]
 
-last_slot = data["last-slot"]
+    if data["last-slot"] is None:
+        data["last-slot"] = 0
+        return {
+            "team-code": state["team-code"],
+            "game": "phase_1",
+            "pull": 0,
+        }
 
-if data["last-slot"] is None:
-	data["last-slot"] = 0
-	return {
-	"team-code": state["team-code"],
-	"game": "phase_1",
-	"pull": 0,
-	}
+    if len(data["slots"][last_slot][0]) == 0:
+        data["slots"][last_slot][0].append(state["last-metadata"])
 
-if len(data["slots"][last_slot][0]) == 0:
-	data["slots"][last_slot][0].append(state["last-metadata"])
+    if len(data["slots"][last_slot][1]) == 0:
+        data["slots"][last_slot][1].append(state["last-cost"])
 
-if len(data["slots"][last_slot][1]) == 0:
-	data["slots"][last_slot][1].append(state["last-cost"])
+    data["slots"][last_slot][2].append(state["last-payoff"])
 
-data["slots"][last_slot][2].append(state["last-payoff"])
+    if last_slot < 20:
+        if data["ctr"] < 30:
+            data["ctr"]+=1
+        else:
+            last_slot += 1
+            data["last-slot"] = last_slot
+            data["ctr"] = 0
+        ret = {
+            "team-code": state["team-code"],
+            "game": "phase_1",
+            "pull": last_slot,
+        }
+    elif last_slot == 99:
+        data["end-explore"] = 1
+        for i in range(20):
+            u = data["slots"][i][1][0]
+            x = data["slots"][i][2]
+            a,b,c,d = beta.fit(x) #0 for min?
+            m = beta.mean(a,b,c,d)
+            v = beta.var(a,b,c,d)
+            if m - u > v**.5:
+                data["slots"][i][0].append(1) #profitable
+            else:
+                data["slots"][i][0].append(0) #unprofitable
+            run_regression()
+            identify()
 
-if last_slot < 20:
-	if data["ctr"] < 30:
-		data["ctr"]+=1
-	else:
-		last_slot += 1
-		data["last-slot"] = last_slot
-		data["ctr"] = 0
-	ret = {
-		"team-code": state["team-code"],
-		"game": "phase_1",
-		"pull": last_slot,
-		}
-elif last_slot == 99:
-	data["end-explore"] = 1
-	for i in range(20):
-		u = data["slots"][i][1][0]
-		x = data["slots"][i][2]
- 		a,b,c,d = beta.fit(x) #0 for min? 
- 		m = beta.mean(a,b,c,d)
- 		v = beta.var(a,b,c,d)
- 		if m - u > v**.5:
- 			data["slots"][i][0].append(1) #profitable
- 		else:
- 			data["slots"][i][0].append(0) #unprofitable
- 		run_regression()
- 		identify()
+            ret = {
+            "team-code": state["team-code"],
+            "game": "phase_1",
+            "pull": ""#profitable as identified
+            }
 
- 		ret = {
-		"team-code": state["team-code"],
-		"game": "phase_1",
-		"pull": #profitable as identified
-		}
+    else:
+        last_slot += 1
+        data["last-slot"] = last_slot
+        if last_slot < 100:
+            ret = {
+            "team-code": state["team-code"],
+            "game": "phase_1",
+            "pull": last_slot,
+            }
+        return ret
 
-else:
-	last_slot += 1
-	data["last-slot"] = last_slot
-	if last_slot < 100:
-		ret = {
-		"team-code": state["team-code"],
-		"game": "phase_1",
-		"pull": last_slot,
-		}
-
-return ret
-
-
-run_regression():
+def run_regression():
+    return None
 #build logistic model for first 20 slot machines
 
-identify():
+def identify():
+    return None
 #assign profitable/unprofitable to other 80 machines based on model
 
 #then pull the 5%??? most profitable
