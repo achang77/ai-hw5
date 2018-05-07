dc5369189bbfee0ea5794ba02a19a7204a584397
romanZwang <rwang2006@yahoo.com>
2018-05-07 17:46:19 -0400

    Restructured code to account for fact that Freenor's program will call get_move

--- hw5.py
+++ hw5.py
@@ -1,4 +1,4 @@
-import scipy.stats.beta
+import scipy
 import numpy as np
 from sklearn import linear_model
 
@@ -24,12 +24,27 @@ state = {"team-code": "eef8976e",
 "last-metadata": "00110101"}
 
 # where we store our data and models
-data= {
-"slots": [ [ [], [], [], [], [] ] for x in range(100) ],
-"last-slot":None,
-"ct":0,
-"our_utility":1000000,
-"end-explore":0
+data = {
+    "slots": [[[], [], [], [], []] for x in range(100)],
+    "turn": 0,
+    "last_slot": None
+}
+
+parameters = {
+    "high_samples": 30,
+    "medium_samples": 20,
+    "low_samples": 10,
+    "slots_high_sample": range(70, 80),
+    "slots_medium_sample": range(80, 100),
+    "slots_low_sample": range(0, 70),
+    "best_n_to_pull": 10,
+    "best_n_to_pull_trials": 10,
+}
+
+switches = {
+    "high_switch": parameters["high_samples"]*len(parameters["slots_high_sample"]),
+    "medium_switch": parameters["medium_samples"]*len(parameters["slots_medium_sample"]) + parameters["high_samples"]*len(parameters["slots_high_sample"]),
+    "low_switch": parameters["low_samples"]*len(parameters["slots_low_sample"]) + parameters["medium_samples"]*len(parameters["slots_medium_sample"]) + parameters["high_samples"]*len(parameters["slots_high_sample"])
 }
 
 #Slot info class - too ambitious
@@ -54,45 +69,34 @@ data= {
 
 #pull n-th slot machine and save data
 
-def pull_n_save(n):
+def set_last_slot_and_ret_slot(n):
+    data["last_slot"] = n
     return {
         "team-code": state["team-code"],
         "game": "phase_1",
         "pull": n
     }
-    load = load_data()
-    # store last metadata string
-    data["slots"][n][0].append(load["last-metadata"])
-    # store last cost
-    data["slots"][n][1].append(load["last-cost"])
-    # store last payoff
-    data["slots"][n][2].append(load["last-payoff"])
-    data["ct"] = ct+1
-
-def phase1a(slots_to_train,y):
-    # x is slot machine number and y is number of trials per slot machine
-    for i in slots_to_train:
-        for j in range(y):
-            pull_n_save(i)
-
 
 def update_beta_coeff(slots_to_train):
     for i in slots_to_train:
         data["slots"][i][3] = np.mean(data["slots"][i][1][0])
         data["slots"][i][4] = scipy.stats.beta.fit(data["slots"][i][2][0])
 
-def add_beta_model_vars(slots_to_train, beta_model_vars, times):
+
+def prep_linear_model_data(slots_to_train, beta_model_vars, times):
     for h in range(times):
         for i in slots_to_train:
             #append slot machine number, slot machine 8 bits separated into 1s and 0s,
             beta_model_vars.append(i, data["slots"][i][0][0], data["slots"][i][0][1], data["slots"][i][0][2], data["slots"][i][0][3], data["slots"][i][0][4], data["slots"][i][0][5], data["slots"][i][0][6], data["slots"][i][0][7], data["slots"][i][4][0], data["slots"][i][4][1], data["slots"][i][4][2], data["slots"][i][4][3])
+
 
 def phase1b(slots_to_train, beta_model_vars, times):
     # create beta prediction models list
     # parse metadata to 8-bit binary variables when creating beta model variable data
 
     update_beta_coeff(slots_to_train)
-    add_beta_model_vars(slots_to_train, beta_model_vars, times)
+    prep_linear_model_data(slots_to_train, beta_model_vars, times)
+
 
 def phase1c(beta_model_vars, beta_models):
 
@@ -107,7 +111,8 @@ def phase1c(beta_model_vars, beta_models):
 
     beta_models = {'alpha': alpha.fit(predictors, beta_model_vars[:, 9]), 'beta': beta.fit(predictors, beta_model_vars[:, 10]), 'loc': loc.fit(predictors, beta_model_vars[:, 11]), 'scale': scale.fit(predictors, beta_model_vars[:, 12])}
 
-def phase1d(beta_models, slots_to_pull):
+
+def predict_and_ret_best_slots(beta_models, slots_to_pull):
     for i in range(0, len(data["slots"])):
         alpha_expected = beta_models['alpha'].predict([data["slots"][i][0][0], data["slots"][i][0][1], data["slots"][i][0][2], data["slots"][i][0][3], data["slots"][i][0][4], data["slots"][i][0][5], data["slots"][i][0][6], data["slots"][i][0][7]])
         beta_expected = beta_models['beta'].predict([data["slots"][i][0][0], data["slots"][i][0][1], data["slots"][i][0][2], data["slots"][i][0][3], data["slots"][i][0][4], data["slots"][i][0][5], data["slots"][i][0][6], data["slots"][i][0][7]])
@@ -120,43 +125,53 @@ def phase1d(beta_models, slots_to_pull):
         slots_to_pull.append(i, 0.3 * bm_expected + 0.7 * mean_expected - cost)
     sorted(slots_to_pull, key=lambda x: x[1])
 
-def phase1(state):
-    ### to do ###
-    # Add if condition to not pull if too many negative expected payouts
+def build_models(beta_model_vars, beta_models):
+    phase1b(parameters["slots_high_sample"], beta_model_vars, 3)
+    phase1b(parameters["slots_medium_sample"], beta_model_vars, 2)
+    phase1b(parameters["slots_low_sample"], beta_model_vars, 1)
 
-    last_slot = data["last-slot"]
+    phase1c(beta_model_vars, beta_models)
 
-    #model alpha, beta, and scale variable for x number of slot machines and y number of trials
-    ### to-do ###
-    # run phase1a on randomly selected slot machines. People will tend to train on first n slot machines, so we want to take advantage of lesser known slot machines.
-
-    slots_high_sample = range(70, 79)
-    slots_medium_sample = range(80, 99)
-    slots_low_sample = range(0, 69)
-    slots_all = range(0, 99)
-
-    # slots_to_predict = range(0,99)
-    # #create list of slots not in train by removing items in 0-99 range list
-    # for i in len(slots_to_predict):
-    #     if i in slots_to_train:
-    #         slots_to_predict.remove(i)
-
-
-    phase1a(slots_high_sample, 30)
-    phase1a(slots_medium_sample, 20)
-    phase1a(slots_low_sample, 10)
+def rank_slots():
 
     beta_model_vars = None
     beta_models = None
     next_slots_to_pull = None
 
-    phase1b(slots_high_sample, beta_model_vars, 3)
-    phase1b(slots_medium_sample, beta_model_vars, 2)
-    phase1b(slots_low_sample, beta_model_vars, 1)
+    build_models(beta_model_vars, beta_models)
+    predict_and_ret_best_slots(beta_models, next_slots_to_pull)
 
-    phase1c(beta_model_vars, beta_models)
-    phase1d(beta_models, next_slots_to_pull)
+    return next_slots_to_pull
 
+
+def get_move():
+    data["turn"] = data["turn"] + 1
+    n = data["last_slot"]
+
+    if(data["turn"]==0):
+        return {
+            "team-code": state["team-code"],
+            "game": "phase_1",
+            "pull": parameters["slots_high_sample"][0]
+        }
+
+    load = load_data()
+    # store last metadata string
+    data["slots"][n][0].append(load["last-metadata"])
+    # store last cost
+    data["slots"][n][1].append(load["last-cost"])
+    # store last payoff
+    data["slots"][n][2].append(load["last-payoff"])
+
+    # pull each high sample slots n times
+    if data["turn"] < switches["high_switch"]:
+        return set_last_slot_and_ret_slot(data["turn"] % parameters["high_samples"])
+
+    if data["turn"] < switches["medium_switch"]:
+        return set_last_slot_and_ret_slot(data["turn"] % parameters["high_samples"])
+
+    if data["turn"] < switches["low_switch"]:
+        return set_last_slot_and_ret_slot(data["turn"] % parameters["low_samples"])
 
 
 # def run_regression():
