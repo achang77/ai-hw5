973faf5c199819b04950be8053120bafc9965747
romanZwang <rwang2006@yahoo.com>
2018-05-07 23:54:43 -0400

    Added get move from state interface Added cost to slot_machines Fixed import statements

--- driver.py
+++ driver.py
@@ -1,12 +1,10 @@
-from hw5 import *
+import hw5
+import numpy as np
 from itertools import product
-import string
+from random import randint
 
 
-def load_data():
-    return None
-
-
+# two bit to integer + .5
 def bit_to_int(tbn):
     return 2*int(tbn[0])+int(tbn[1])+.5
 
@@ -21,6 +19,7 @@ if __name__ == "__main__":
         "beta": [],
         "loc": [],
         "scale": [],
+        "cost": [],
         "ebn": []
     }
     for ebn in product('01', repeat=8):
@@ -32,3 +31,14 @@ if __name__ == "__main__":
         slot_machines["beta"].append(bit_to_int(i[2:4]))
         slot_machines["loc"].append(bit_to_int(i[4:6]))
         slot_machines["scale"].append(bit_to_int(i[6:8]))
+        slot_machines["cost"].append(randint(0, 9))
+
+    prev_slot = hw5.get_move()
+    for i in range(9999):
+        state = {"team-code": "eef8976e",
+            "game": "phase_1",
+            "pulls-left": 10000-i,
+            "last-cost": slot_machines["cost"][prev_slot],
+            "last-payoff": get_rand_beta_var(prev_slot, slot_machines),
+            "last-metadata": slot_machines["ebn"][prev_slot]}
+        prev_slot = hw5.get_move(state)
