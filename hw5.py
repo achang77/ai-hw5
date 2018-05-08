998b6dafa38056fab49036f10cbf40af49b8c11f
romanZwang <rwang2006@yahoo.com>
2018-05-07 21:02:33 -0400

    Eliminated unnecessary parameters and added return statements in helper functions

--- hw5.py
+++ hw5.py
@@ -69,6 +69,7 @@ switches = {
 
 #pull n-th slot machine and save data
 
+
 def set_last_slot_and_ret_slot(n):
     data["last_slot"] = n
     return {
@@ -76,6 +77,7 @@ def set_last_slot_and_ret_slot(n):
         "game": "phase_1",
         "pull": n
     }
+
 
 def update_beta_coeff(slots_to_train):
     for i in slots_to_train:
@@ -98,7 +100,7 @@ def phase1b(slots_to_train, beta_model_vars, times):
     prep_linear_model_data(slots_to_train, beta_model_vars, times)
 
 
-def phase1c(beta_model_vars, beta_models):
+def phase1c(beta_model_vars):
 
     # generate beta coefficient prediction model from binary metadata predictors
 
@@ -109,10 +111,11 @@ def phase1c(beta_model_vars, beta_models):
     loc = linear_model.LinearRegression()
     scale = linear_model.LinearRegression()
 
-    beta_models = {'alpha': alpha.fit(predictors, beta_model_vars[:, 9]), 'beta': beta.fit(predictors, beta_model_vars[:, 10]), 'loc': loc.fit(predictors, beta_model_vars[:, 11]), 'scale': scale.fit(predictors, beta_model_vars[:, 12])}
+    return {'alpha': alpha.fit(predictors, beta_model_vars[:, 9]), 'beta': beta.fit(predictors, beta_model_vars[:, 10]), 'loc': loc.fit(predictors, beta_model_vars[:, 11]), 'scale': scale.fit(predictors, beta_model_vars[:, 12])}
 
 
-def predict_and_ret_best_slots(beta_models, slots_to_pull):
+def predict_and_ret_best_slots(beta_models):
+    slots_to_pull = []
     for i in range(0, len(data["slots"])):
         alpha_expected = beta_models['alpha'].predict([data["slots"][i][0][0], data["slots"][i][0][1], data["slots"][i][0][2], data["slots"][i][0][3], data["slots"][i][0][4], data["slots"][i][0][5], data["slots"][i][0][6], data["slots"][i][0][7]])
         beta_expected = beta_models['beta'].predict([data["slots"][i][0][0], data["slots"][i][0][1], data["slots"][i][0][2], data["slots"][i][0][3], data["slots"][i][0][4], data["slots"][i][0][5], data["slots"][i][0][6], data["slots"][i][0][7]])
@@ -121,27 +124,25 @@ def predict_and_ret_best_slots(beta_models, slots_to_pull):
         bm_expected = scipy.stats.beta.mean(alpha_expected, beta_expected, loc_expected, scale_expected)
         mean_expected = np.mean(data["slots"][11][i])
         cost = data["slots"][i][1]
-        # weight actual means higher than averages. model is included because it may provide insight into high impact low probability payout vallues.
+        # weight actual means higher than averages
+        # model is included because it may provide insight into high impact low probability payout values
         slots_to_pull.append(i, 0.3 * bm_expected + 0.7 * mean_expected - cost)
     sorted(slots_to_pull, key=lambda x: x[1])
+    return slots_to_pull
 
-def build_models(beta_model_vars, beta_models):
+
+def build_models():
+    beta_model_vars = []
     phase1b(parameters["slots_high_sample"], beta_model_vars, 3)
     phase1b(parameters["slots_medium_sample"], beta_model_vars, 2)
     phase1b(parameters["slots_low_sample"], beta_model_vars, 1)
 
-    phase1c(beta_model_vars, beta_models)
+    return phase1c(beta_model_vars)
+
 
 def rank_slots():
-
-    beta_model_vars = None
-    beta_models = None
-    next_slots_to_pull = None
-
-    build_models(beta_model_vars, beta_models)
-    predict_and_ret_best_slots(beta_models, next_slots_to_pull)
-
-    return next_slots_to_pull
+    beta_models = build_models()
+    return predict_and_ret_best_slots(beta_models)
 
 
 def get_move():
@@ -173,65 +174,4 @@ def get_move():
     if data["turn"] < switches["low_switch"]:
         return set_last_slot_and_ret_slot(data["turn"] % parameters["low_samples"])
 
-
-# def run_regression():
-#     return None
-# #build logistic model for first 20 slot machines
-#
-# def identify():
-#     return None
-# #assign profitable/unprofitable to other 80 machines based on model
-# #then pull the 5%??? most profitable
-# #also consider track utility? if utility drops below threshold then break-out?
-
-# if len(data["slots"][last_slot][0]) == 0:
-    #     data["slots"][last_slot][0].append(state["last-metadata"])
-    #
-    # if len(data["slots"][last_slot][1]) == 0:
-    #     data["slots"][last_slot][1].append(state["last-cost"])
-    #
-    # data["slots"][last_slot][2].append(state["last-payoff"])
-    #
-    # if last_slot < 20:
-    #     if data["ctr"] < 30:
-    #         data["ctr"]+=1
-    #     else:
-    #         last_slot += 1
-    #         data["last-slot"] = last_slot
-    #         data["ctr"] = 0
-    #     ret = {
-    #         "team-code": state["team-code"],
-    #         "game": "phase_1",
-    #         "pull": last_slot,
-    #     }
-    # elif last_slot == 99:
-    #     data["end-explore"] = 1
-    #     for i in range(20):
-    #         u = data["slots"][i][1][0]
-    #         x = data["slots"][i][2]
-    #         a,b,c,d = beta.fit(x) #0 for min?
-    #         m = beta.mean(a,b,c,d)
-    #         v = beta.var(a,b,c,d)
-    #         if m - u > v**.5:
-    #             data["slots"][i][0].append(1) #profitable
-    #         else:
-    #             data["slots"][i][0].append(0) #unprofitable
-    #         run_regression()
-    #         identify()
-    #
-    #         ret = {
-    #         "team-code": state["team-code"],
-    #         "game": "phase_1",
-    #         "pull": ""#profitable as identified
-    #         }
-    #
-    # else:
-    #     last_slot += 1
-    #     data["last-slot"] = last_slot
-    #     if last_slot < 100:
-    #         ret = {
-    #         "team-code": state["team-code"],
-    #         "game": "phase_1",
-    #         "pull": last_slot,
-    #         }
-    #     return ret
+    next_slots_to_pull = rank_slots()
